59412e8faed0da3c049ee048301759dd2e04240b
Yassin Malik <ml.yassink@gmail.com>
2025-01-07 13:14:22 +0100

    Finishing overall menu and begining map

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,12 +4,10 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button startButton, quitButton, optionButton, backButton;
-    [SerializeField] private GameObject menu, option;
+    [SerializeField] private Button startButton, quitButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        option.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,33 +15,17 @@ public class MainMenu : MonoBehaviour
     {
         startButton.onClick.AddListener(StartGame);
         quitButton.onClick.AddListener(QuitGame);
-        optionButton.onClick.AddListener(Options);
-        backButton.onClick.AddListener(BackButton);
     }
 
     void OnDisable()
     {
         startButton.onClick.RemoveListener(StartGame);
         quitButton.onClick.RemoveListener(QuitGame);
-        optionButton.onClick.RemoveListener(Options);
-        backButton.onClick.RemoveListener(BackButton);
     }
 
     void StartGame()
     {
         SceneManager.LoadScene(1);
-    }
-
-    void Options()
-    {
-        menu.gameObject.SetActive(false);
-        option.gameObject.SetActive(true);
-    }
-
-    void BackButton()
-    {
-        menu.gameObject.SetActive(true);
-        option.gameObject.SetActive(false);
     }
 
     void QuitGame()
